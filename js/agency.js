2a8547e5054be758c15dfea6b3fb425321f11e69
emmadema <emma.demango@gmail.com>
2017-12-06 13:13:02 -0700

    adding new project

--- js/agency.js
+++ js/agency.js
@@ -78,51 +78,51 @@ $("#letters").on('click',function(){
   
 });
 
-function explodeLetters(){
+// function explodeLetters(){
   
-  threeDTimeline = new TimelineLite({align:'start'});
+//   threeDTimeline = new TimelineLite({align:'start'});
   
-  var children = $('.letter').children().length;
-  for(var i=0;i<children;i++){
+//   var children = $('.letter').children().length;
+//   for(var i=0;i<children;i++){
          
-         for(var i=0;i<children;i++){
+//          for(var i=0;i<children;i++){
           
-          var element = $(".letter").children().eq(i);
-          var pos = element.offset();
-          element.css({'left':pos.left,'top':pos.top});
+//           var element = $(".letter").children().eq(i);
+//           var pos = element.offset();
+//           element.css({'left':pos.left,'top':pos.top});
           
-          threeDTimeline.insert(TweenMax.to(element,1.5, {
-          'position':'absolute', 
-          left:Math.random() * 650 - 100, 
-          top:Math.random() * 350 - 100,
-          ease:Quad.easeIn,
-          fontSize: '+=35',
-          autoAlpha:0}));  
-    }
-    threeDTimeline.play();
-  } 
-}
-function splitLetters(userInput){
-  var a;
-  var arr = userInput.split("");
+//           threeDTimeline.insert(TweenMax.to(element,1.5, {
+//           'position':'absolute', 
+//           left:Math.random() * 650 - 100, 
+//           top:Math.random() * 350 - 100,
+//           ease:Quad.easeIn,
+//           fontSize: '+=35',
+//           autoAlpha:0}));  
+//     }
+//     threeDTimeline.play();
+//   } 
+// }
+// function splitLetters(userInput){
+//   var a;
+//   var arr = userInput.split("");
   
-  for(var i=0;i<arr.length;i++) { 
+//   for(var i=0;i<arr.length;i++) { 
     
-    if(arr[i] == " "){
-        arr[i] = '<div class="letter-measure blank">' + arr[i] + '</div>';
-    }
-    else{
+//     if(arr[i] == " "){
+//         arr[i] = '<div class="letter-measure blank">' + arr[i] + '</div>';
+//     }
+//     else{
       
-          if(!arr[i].match(/\s\n\t\r/g) && arr[i]!="") arr[i] = '<div class="letter-measure">' + arr[i] + '</div>';
+//           if(!arr[i].match(/\s\n\t\r/g) && arr[i]!="") arr[i] = '<div class="letter-measure">' + arr[i] + '</div>';
      
-      }
-   }
+//       }
+//    }
    
-   //$(this).html(arr.join(" "));
+//    //$(this).html(arr.join(" "));
    
-   return arr.join(" ");
+//    return arr.join(" ");
   
-}
+// }
 
   })(jQuery); // End of use strict
 });
